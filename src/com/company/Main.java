71f44840a8bf29f85886d7761b12f0a83d314eff
filipsupolik25@gmail.com <14Filip14>
2020-09-29 09:07:47 +0200

    Completed and tested.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter variable a :" );
+        int a = scan.nextInt();
+        System.out.println("Enter variable b:");
+        int b = scan.nextInt();
+        System.out.println("Enter variable c:");
+       int c = scan.nextInt();
+        scan.close();
+
+        if (c<b||c<a)
+            System.out.println(true);
+        else
+            System.out.println(false);
+        if (a+b>c)
+            System.out.println(true);
+        else
+            System.out.println(false);
     }
 }
